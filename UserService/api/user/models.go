746ac3807a01a0b7c28c4fdd08caa66df710d25e
fla-t <salman.mzmil@gmail.com>
2024-12-14 17:26:01 +0500

    proper DDD now

---
+++ UserService/api/user/models.go
@@ -0,0 +1,45 @@
+package user
+
+import (
+	"errors"
+
+	domain "github.com/fla-t/go-ing/UserService/internal/domain/user"
+)
+
+// User represents a user of the system.
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// Validate validates the user.
+func (u *User) Validate() error {
+	if u.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if u.Email == "" {
+		return errors.New("email is required")
+	}
+
+	return nil
+}
+
+// ConvertToDomain converts a user to a domain user.
+func (u *User) ConvertToDomain() *domain.User {
+	return &domain.User{
+		ID:    u.ID,
+		Name:  u.Name,
+		Email: u.Email,
+	}
+}
+
+// ConvertFromDomain converts a domain user to a user.
+func ConvertFromDomain(u *domain.User) *User {
+	return &User{
+		ID:    u.ID,
+		Name:  u.Name,
+		Email: u.Email,
+	}
+}
