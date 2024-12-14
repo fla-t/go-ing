5a0541de55fe9451fc466a15169658adb05bd519
fla-t <salman.mzmil@gmail.com>
2024-12-15 00:52:30 +0500

    gcam "cosmetics"

--- UserService/internal/grpc/user/server.go
+++ UserService/internal/grpc/user/server.go
@@ -8,19 +8,19 @@ import (
 	"github.com/fla-t/go-ing/UserService/proto"
 )
 
-// UserService is the gRPC server for the user service
-type UserService struct {
+// Service is the gRPC server for the user service
+type Service struct {
 	proto.UnimplementedUserServiceServer
 	service *user.Service
 }
 
 // NewUserService creates a new UserService
-func NewUserService(service *user.Service) *UserService {
-	return &UserService{service: service}
+func NewUserService(service *user.Service) *Service {
+	return &Service{service: service}
 }
 
 // GetUser returns a user by its id
-func (s *UserService) GetUser(ctx context.Context, req *proto.GetUserRequest) (*proto.GetUserResponse, error) {
+func (s *Service) GetUser(ctx context.Context, req *proto.GetUserRequest) (*proto.GetUserResponse, error) {
 	u, err := s.service.GetUserByID(req.Id)
 	if err != nil {
 		return nil, err
@@ -36,7 +36,7 @@ func (s *UserService) GetUser(ctx context.Context, req *proto.GetUserRequest) (*
 }
 
 // CreateUser creates a new user
-func (s *UserService) CreateUser(ctx context.Context, req *proto.CreateUserRequest) (*proto.CreateUserResponse, error) {
+func (s *Service) CreateUser(ctx context.Context, req *proto.CreateUserRequest) (*proto.CreateUserResponse, error) {
 	u := &domain.User{
 		ID:    req.GetId(),
 		Name:  req.GetName(),
@@ -51,7 +51,7 @@ func (s *UserService) CreateUser(ctx context.Context, req *proto.CreateUserReque
 }
 
 // DeleteUser deletes a user by its id
-func (s *UserService) DeleteUser(ctx context.Context, req *proto.DeleteUserRequest) (*proto.DeleteUserResponse, error) {
+func (s *Service) DeleteUser(ctx context.Context, req *proto.DeleteUserRequest) (*proto.DeleteUserResponse, error) {
 	err := s.service.DeleteUser(req.Id)
 	if err != nil {
 		return nil, err
